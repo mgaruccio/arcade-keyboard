a9fd4770c5674377616b269abe4a8764232b950b
Simon Prickett <simon@crudworks.org>
2020-07-11 18:14:23 -0700

    Adds cycling LED effect.

--- arcadekeyboard/arcadekeyboard.ino
+++ arcadekeyboard/arcadekeyboard.ino
@@ -1,3 +1,4 @@
+#include <Adafruit_DotStar.h>
 #include <Bounce2.h>
 #include <Keyboard.h>
 
@@ -6,16 +7,35 @@
 
 Bounce debouncer = Bounce();
 
+Adafruit_DotStar strip = Adafruit_DotStar(1, INTERNAL_DS_DATA, INTERNAL_DS_CLK, DOTSTAR_BGR);
+
+uint32_t color = 0xFF0000;
+unsigned long prevMillis = 0;
+
 void setup() {
   debouncer.attach(BUTTON_PIN, INPUT_PULLUP);
   debouncer.interval(30);
   pinMode(LED_PIN, OUTPUT);
   Keyboard.begin();
+  strip.begin();
 }
  
 // the loop function runs over and over again forever
 void loop() {
   debouncer.update();
+
+  unsigned long nowMillis = millis();
+
+  if (nowMillis - prevMillis >= 1000) {
+    prevMillis = nowMillis;
+    
+    if((color >>= 8) == 0) {
+      color = 0xFF0000; 
+    }
+
+    strip.setPixelColor(0, color); 
+    strip.show();    
+  }
 
   if (debouncer.fell()) {
     digitalWrite(LED_PIN, HIGH);
